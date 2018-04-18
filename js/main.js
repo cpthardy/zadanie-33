f3ab65eb52b7ea830ceaf6854e4a73d266658ae0
Maciej Wydrych <m.wydrych@gmail.com>
2018-04-18 20:40:54 +0200

    wykonanie zadania

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var paragraf = document.getElementById("opis");
+var przycisk = document.querySelector("button");
+
+function wstaw() {
+    document.getElementById("opis").innerHTML = "wcisnąłeś przycisk";
+}
